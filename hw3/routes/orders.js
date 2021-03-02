a4361bfde032062190c7cf1afbd69bdc506dbb91
Daniel Co <43159113+co22@users.noreply.github.com>
2021-03-01 22:25:31 -0800

    Added jquery dependency in package.json

---
+++ hw3/routes/orders.js
@@ -0,0 +1,11 @@
+/* Daniel Co */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({ hello: 'world' });
+});
+
+module.exports = router;
